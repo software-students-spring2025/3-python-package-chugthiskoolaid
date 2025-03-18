bc8b38cc87ee386ce9ce323e134b0ff3e24bea54
Hao-Yang-Hao <hy2640@nyu.edu>
2025-03-18 14:17:07 -0400

    add

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -3,18 +3,8 @@ from mood_suggester.activity import recommend_activity, ACTIVITY_RECOMMENDATIONS
 
 def test_recommend_activity_valid_mood():
 
-    """ activities = {
-        "happy": ["Go for a walk in the park", "Attend a friend's party", "Try out new hobbies"],
-        "sad": ["Write in a journal", "Meditate"],
-        "stressed": ["Do deep breathing exercises", "Practice yoga", "Drink a hot cup of tea"],
-        "bored": ["Learn a new skill", "Watch a documentary"]
-        "motivated": ["Set a new goal and plan out the steps to achieve it", 
-                  "Hit the gym or do an intense workout"]
-        "angry": ["Do a high-intensity workout or go for a run", 
-              "Write down your thoughts, then tear up the paper or burn it safely"]} 
-    """
 
-    moods = ["happy", "sad", "stressed", "bored", "motivated","angry"]
+    moods = ["happy", "sad", "stressed", "bored","motivated","angry"]
     for mood in moods:
         activity = recommend_activity(mood)
         assert activity in ACTIVITY_RECOMMENDATIONS[mood]
--- tests/test_movie.py
+++ tests/test_movie.py
@@ -2,11 +2,10 @@ import pytest
 from mood_suggester.movie import recommend_movie, MOVIE_RECOMMENDATIONS
 
 def test_recommend_movie():
-    moods = ["happy", "sad", "stressed", "bored", "motivated","angry"]
+    moods = ["Happy", "Sad", "Stressed", "Bored","Motivated","Angry"]
     for mood in moods:
         movie = recommend_movie(mood)
         assert movie in MOVIE_RECOMMENDATIONS[mood]
 
 def test_invalid_mood():
-=======
     assert recommend_movie("") == "No recommendations available for this mood."
